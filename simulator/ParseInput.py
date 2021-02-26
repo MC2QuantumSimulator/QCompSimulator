da10e8cbe1875aa346c697a63d8656dac131922c
shekka <shekka@student.chalmers.se>
2021-02-26 17:06:13 +0100

    Continued work on parse_gates method to parse the gates file into a matrix

--- simulator/ParseInput.py
+++ simulator/ParseInput.py
@@ -2,15 +2,28 @@ import numpy as np
 import math
 
 
+
 class ParseInput:
 
-    def print(self):
-        print("hej Naomi")
 
-    def parse_gates(self, text_file):
-        f = open('input.txt', 'r')
-        matrix_string_list = f.readlines()
+    def parse_gates(test_file):
+        f = open(test_file, 'r')
+        gates_string_list = f.readlines()
+        gate_names = []
+
+        for gate in gates_string_list:
+            gate = ''.join(gate.split()) #remove whitespaces
+            split = gate.split('=')
+            gate_names.append(split[0])
+            split[1] #TODO how to parse the matrix part of gate
 
 
-if __name__ == '__main__':
-    print(eval("3+1"))
+
+
+
+
+
+    if __name__ == '__main__':
+        parse_gates('C:/Users/naomi/Documents/Kod projekt/PycharmProjects/inputFiles/gates.txt')
+
+
