f0df015d01df43a20ba05c0e628538e37e20e852
StonesmileGit <62522391+StonesmileGit@users.noreply.github.com>
2021-03-09 16:11:12 +0100

    change np.matrix to np.array

--- simulator/test_qvector.py
+++ simulator/test_qvector.py
@@ -14,14 +14,14 @@ class TestQvector(unittest.TestCase):
         pass
 
     def test_mult(self):
-        matrix_tree = qmatrix.to_tree(np.matrix(
+        matrix_tree = qmatrix.to_tree(np.array(
         [[1, 2, 7, 2, 4, 5, 3, 2], [4, 7, 8, 3, 5, 4, 1, 9], [1, 7, 45, 6, 8, 9, 4, 7], [1, 6, 2, 3, 4, 4, 3, 2],
          [1, 2, 7, 1, 4, 5, 7, 2], [4, 2, 8, 4, 5, 4, 1, 9], [1, 7, 89, 6, 5, 9, 4, 7], [1, 6, 6, 1, 4, 7, 3, 2]]))
         vector_tree = qvector.to_tree((1, 6, 8, 3, 5, 4, 6, 1))
         self.assertTrue(np.array_equal(qvector.mult(matrix_tree,vector_tree).to_vector(),
                                        np.matmul(matrix_tree.to_matrix(),vector_tree.to_vector())))
     def test_mult1(self):
-        matrix_tree=qmatrix.to_tree(np.matrix(
+        matrix_tree=qmatrix.to_tree(np.array(
         [[1,6,4,2],[7,3,357,57],[1,264,6,7],[67,3,2,1]]))
         vector_tree = qvector.to_tree((2,7,5,43))
         self.assertTrue(np.array_equal(qvector.mult(matrix_tree, vector_tree).to_vector(),
