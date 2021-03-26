4ce39dae8350a7f8dbc2c60db8560b2737e5e77d
ErikNils <erik1.nilsson2@gmail.com>
2021-03-26 02:26:40 +0100

    Interpreter now works for gates of size 2 (only Hadamard gate tested)

--- simulator/interpreter.py
+++ simulator/interpreter.py
@@ -15,9 +15,15 @@ def qreg(n):
     
     return qvector.to_tree((np.array(vector)))
 
-# Gets the int between '[]'
-def get_int(split):
-    return eval(split[1].split('[')[1].split(']')[0])
+# Gets the index of the qbit between '[]'. 
+# Returns '-1' if a gate should be applied to all qbits
+def get_int(str):
+    ev = str.split('[')
+
+    if len(ev) == 1: ev = -1
+    else: ev = eval(ev[1].split(']')[0])
+
+    return ev
 
 
 def parse_qasm(qasm_file):
@@ -53,7 +59,7 @@ def parse_qasm(qasm_file):
 
         # Creates quantum register of n bits
         if var == 'qreg':
-            q = qreg(get_int(split))
+            q = qreg(get_int(split[1]))
         # If the gate is supported append var and op
         elif var in gate_names: 
             variables.append(split[0])
@@ -62,31 +68,75 @@ def parse_qasm(qasm_file):
         elif var not in headers:
             print(var + " is not supported")
             continue
-    # Only works for whole vector and not single qbits
-    # TODO: Få det att funka bitvis
-    for var in variables:
-        i = gate_names.index(var)
-        mat = np.array(gate_matrix[i])
-        #print(mat)
-        size = int((len(qvector.to_vector(q)))/2) # Required size of identity matrix
-        tree1 = qmatrix.to_tree(mat) # 
-        tree2 = qmatrix.to_tree(np.identity(size))
-        p = qmatrix.kron(tree1, tree2)
-        q = qvector.mult(p, q) # TODO: Väldigt säker på att resultatet blir fel
+
+    for index, var in enumerate(variables):
+        ivar = gate_names.index(var)
+        gate = qmatrix.to_tree(np.array(gate_matrix[ivar])) 
+        qbit = get_int(operations[index])
+        p = None
+
+        # Applies a gate to every qbit
+        if qbit == -1: 
+            for i in range(q.height):
+                p = gatepadding(gate, i, q.height)
+                q = qvector.mult(p, q)
+                #print(q.to_vector())
+                gate = qmatrix.to_tree(np.array(gate_matrix[ivar])) 
+        else:
+            #print(gate.to_matrix())
+            #print(qbit)
+            #print(q.height)
+            p = gatepadding(gate, qbit, q.height)
+            #print(p.to_matrix())
+            q = qvector.mult(p, q) 
+        
+        
     #print(variables)
     #print(operations)
     print(q.to_vector())
 
 def gatepadding(gate: qmatrix, pre_n: int, tot_len: int) -> qmatrix:
     "Return a circuit layer created from a single gate"
-    # Prepend gate with correct sized identity matrix
-    pre = qmatrix.id(pre_n)
-    # append identity matrix to fill up to same size as qreg
-    post = qmatrix.id(tot_len-pre_n-gate.height)
-    # Kron the trees together
-    result_pre = qmatrix.kron(pre, gate)
-    result = qmatrix.kron(result_pre, post)
-    return result
+    
+    height = gate.height
+    
+    # Size of identity gate before gate represents which qbit the gate should be applied on
+    # Identity matrix of size 2^n where n is the qbit the gate should be applied on
+    if pre_n != 0:
+        pre = qmatrix.id(pre_n)
+        gate = qmatrix.kron(pre, gate)
+
+    # Append identity matrix to fill up to same size as qreg
+    if pre_n != tot_len-1:
+        post = qmatrix.id(tot_len-pre_n-height)
+        gate = qmatrix.kron(gate, post)
+    
+    return gate
+
+def gate_on_qubit(vector: qvector, gate: qmatrix, qbit: int, qreg_size: int) -> qvector:
+    print(qbit)
+    if qbit != 0:
+        identity1 = qmatrix.id(1)
+        #print(identity1)
+        print(range(qbit))
+        for i in range(qbit):
+            identity1 = qmatrix.kron(identity1, qmatrix.id(1))
+
+    identity2 = qmatrix.id(1)
+    print(range(qbit, qreg_size-1))
+    for i in range(qbit, qreg_size-1):
+        print(i)
+        print(identity2.to_matrix())
+        identity2 = qmatrix.kron(identity2, qmatrix.id(1))
+
+    if qbit != 0: qmatrix.kron(identity1, gate)
+    
+    print(gate.to_matrix())
+    print(identity2.to_matrix())
+    kron = qmatrix.kron(gate, identity2)
+
+    return qvector.mult(kron, vector)
+        
 
 if __name__ == '__main__':
     abs_qasm = os.path.join(os.path.dirname(__file__), "../inputFiles/qasm.txt")
