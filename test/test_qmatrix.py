f2d45c744de493ae8f306c7125a76ea37f8bc69b
Rayinfluense <samuel125807@hotmail.com>
2021-03-29 15:11:38 +0200

    More tests for matrix mult

--- test/test_qmatrix.py
+++ test/test_qmatrix.py
@@ -59,13 +59,28 @@ class TestQmatrix(unittest.TestCase):
                                     np.matmul(first.to_matrix(), second.to_matrix())))
 
     def test_mult3(self):
-        eightid = qmatrix.to_tree(np.array(
-            [[1, 0, 0, 0, 0, 0, 0, 0], [0, 1, 0, 0, 0, 0, 0, 0], [0, 0, 1, 0, 0, 0, 0, 0], [0, 0, 0, 1, 0, 0, 0, 0],
-             [0, 0, 0, 0, 1, 0, 0, 0], [0, 0, 0, 0, 0, 1, 0, 0], [0, 0, 0, 0, 0, 0, 1, 0], [0, 0, 0, 0, 0, 0, 0, 1]]))
         twoid = qmatrix.to_tree(np.array([[1, 0], [0, 1]]))
         had = qmatrix.to_tree(np.array(
             [[1, 1], [1, -1]]))
         fourid = qmatrix.to_tree(np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]))
+        first = qmatrix.to_tree(np.array(
+            [[1, 2, 7, 2, 4, 5, 3, 2], [4, 7, 8, 3, 5, 4, 1, 9], [1, 7, 45, 6, 8, 9, 4, 7], [1, 6, 2, 3, 4, 4, 3, 2],
+             [1, 2, 7, 1, 4, 5, 7, 2], [4, 2, 8, 4, 5, 4, 1, 9], [1, 7, 89, 6, 5, 9, 4, 7], [1, 6, 6, 1, 4, 7, 3, 2]]))
+        second = qmatrix.to_tree(np.array(
+            [[54, 7, 8, 2, 23, 5, 7, 8], [6, -43, 3, 9, -7, 6, 4, 2], [76, -8, 3, 2, 43, 8, -9, 3],
+             [6, -9, 3, 3, 1, 7, 54, 2354],
+             [67, -8, 3, 45, 8, 8, 5, 2], [7, 4, 4, 2, 6, 87, 9, 54], [7, 32, 5, 8, 9, 5, 432, 12],
+             [6, 8, 56, 3, 2, 6, 8, 9]]))
+        eightid = qmatrix.to_tree(np.array(
+            [[1, 0, 0, 0, 0, 0, 0, 0], [0, 1, 0, 0, 0, 0, 0, 0], [0, 0, 1, 0, 0, 0, 0, 0], [0, 0, 0, 1, 0, 0, 0, 0],
+             [0, 0, 0, 0, 1, 0, 0, 0], [0, 0, 0, 0, 0, 1, 0, 0], [0, 0, 0, 0, 0, 0, 1, 0], [0, 0, 0, 0, 0, 0, 0, 1]]))
+        third = qmatrix.to_tree(np.array(
+            [[1, 6, 4, -2], [7, 3, 357, 57], [-1, 264, 6, 7], [67, -3, 2, 1]]))
+        self.assertTrue(np.allclose(qmatrix.mult(eightid, first).to_matrix(), np.matmul(eightid.to_matrix(), first.to_matrix())))
+        self.assertTrue(np.allclose(qmatrix.mult(first, eightid).to_matrix(), np.matmul(first.to_matrix(), eightid.to_matrix())))
+        self.assertTrue(np.allclose(qmatrix.mult(second, eightid).to_matrix(), np.matmul(second.to_matrix(), eightid.to_matrix())))
+        self.assertTrue(np.allclose(qmatrix.mult(eightid, second).to_matrix(), np.matmul(eightid.to_matrix(), second.to_matrix())))
+        self.assertTrue(np.allclose(qmatrix.mult(third, fourid).to_matrix(), np.matmul(third.to_matrix(), fourid.to_matrix())))
         res = qmatrix.kron(had, fourid)
         fourid = qmatrix.to_tree(np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]))
         had = qmatrix.to_tree(np.array(
